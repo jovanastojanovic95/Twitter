a66eed1bef4d465b86d18e3a2d2abb41311356b0
jovanastojanovic95 <jstojanovic95@gmail.com>
2016-03-28 01:58:33 +0200

    Dokumentovana klasa TwitterPoruka.

--- Twitter/src/com/twitter/poruke/TwitterPoruka.java
+++ Twitter/src/com/twitter/poruke/TwitterPoruka.java
@@ -1,29 +1,53 @@
 package com.twitter.poruke;
 
+/**
+  *Ovo je klasa koja sadrzi dva atributa i predstavlja poruku na Twitteru.
+ * @author Jovana Stojanovic
+ */
 public class TwitterPoruka {
+	/**
+	 * Ovaj atribut predstavlja ime korisnika.
+	 */
 	private String korisnik;
+	/**
+	 * Ovaj atribut predstavlja zeljenu poruku.
+	 */
 	private String poruka;
-
+/**
+ * Ova metoda vraca ime korisnika
+ * @return ime korisnika
+ */
 	public String getKorisnik() {
 		return korisnik;
 	}
-
+/**
+ * Ova metoda postavlja odgovarajuce ime korisnika
+ * @param korisnik ime korisnika
+ */
 	public void setKorisnik(String korisnik) {
 		if (korisnik == null || korisnik == "")
 			throw new RuntimeException("Ime korisnika mora biti uneto");
 		this.korisnik = korisnik;
 	}
-
+/**
+ * Ova metoda vraca odgovarajucu poruku.
+ * @return odgovarajuca poruka
+ */
 	public String getPoruka() {
 		return "poruka";
 	}
-
+/**
+ * Ova metoda postavlja vrednost atributa poruka na unetu poruku.
+ * @param poruka odgovarajuca poruka
+ */
 	public void setPoruka(String poruka) {
 		if (poruka == null || this.poruka.length() > 140)
 			throw new RuntimeException("Poruka mora biti uneta i mora imati najvise 140 znakova");
 		this.poruka = poruka;
 	}
-
+/**
+ * Ova metoda vraca vrednosti oba atributa.
+ */
 	public String toString() {
 		return "KORISNIK:" + korisnik + " PORUKA:" + poruka;
 	}
