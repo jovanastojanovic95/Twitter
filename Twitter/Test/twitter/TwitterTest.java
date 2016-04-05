09be39f11db67189eb2703acebcc09fe0eb4c48d
jovanastojanovic95 <jstojanovic95@gmail.com>
2016-04-05 16:00:03 +0200

    Napravljen test za klasu Twitter u okviru source foldera test, paket twitter. Za sada testovi nefunkcionalni.

---
+++ Twitter/Test/twitter/TwitterTest.java
@@ -0,0 +1,36 @@
+package twitter;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TwitterTest {
+
+	@Before
+	public void setUp() throws Exception {
+		
+		
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testVratiSvePoruke() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUnesi() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testVratiPoruke() {
+		fail("Not yet implemented");
+	}
+
+}
